e5ffad64b9c4e455b08d877680bb3745881da26c
Zulfiya <zulfiyaabdullaeva03@gmail.com>
2023-07-11 10:12:20 +0500

    last commit

--- src/db.service.js
+++ src/db.service.js
@@ -1,6 +1,26 @@
 const fs = require('fs')
 
+const filePath = __dirname + '/db.json'
+
 function getDB(){
-    const buffer = fs.readFileSync(__dirname + '/db.json', 'utf-8')
+    const buffer = fs.readFileSync(filePath, 'utf-8')
     const db = JSON.parse(buffer)
+    return db
 }
+
+function setDB(payload) {
+    const stringify = JSON.stringify(payload)
+    fs.writeFileSync(filePath, stringify, 'utf-8')
+    console.log('Ma`lumot qo`shildi')
+}
+
+function getID(){
+    const db = getDB()
+    const id = db.id + 1
+    setDB({id, users: db.users})
+    return id
+}
+
+exports.getDB = getDB
+exports.setDB = setDB
+exports.getID = getID
--- src/main.js
+++ src/main.js
@@ -1,10 +1,9 @@
 const prompt = require('prompt-sync')()
 const {commands} = require('./commands')
-const info = require('./info')
-
+const {getDB, setDB, getID} = require('./db.service')
+const info = require('./info') 
 
 while(true){
-
     info()
     
     const command = prompt('Buyruqni kiriting: ')
@@ -16,7 +15,20 @@ while(true){
     }
 
     if(addUser){
-        
+        const name = prompt('Foydalanuvchining nomi: ')
+        const surname = prompt('Foydalanuvchining familyasi: ')
+        const age = prompt('Foydalanuvchining yoshi: ')
+
+        const newUser = {
+            id: getID(),
+            name,
+            surname,
+            age 
+        }
+
+        const db = getDB()
+        db.users.push(newUser)
+        setDB(db)
     }
     
 }
