18ab2d9315652892d6cc84170fa2d63d13d1dc37
Zulfiya <zulfiyaabdullaeva03@gmail.com>
2023-07-12 20:58:09 +0500

    edit

--- src/main.js
+++ src/main.js
@@ -10,6 +10,7 @@ while(true) {
     const isExit = commands["Exit"].includes(command)
     const isAddUser = commands["Add User"].includes(command)
     const isDelete = commands["Delete User"].includes(command)
+    const isEdit = commands["Edit User"].includes(command)
     
     if(isExit) {
         break
@@ -38,5 +39,38 @@ while(true) {
         const id = prompt('ID ni kiriting: ')
         const newArray = db.users.filter((user) => user.id !== +id)
         setDB({ id: db.id, users: newArray})
+    }
+
+    if(isEdit) {
+        const db = getDB()
+        console.log(db.users);
+        const id = prompt('ID ni kiriting: ')
+
+        const user = db.users.find((u) => u.id === +id)
+
+        if(!user){
+            return
         }
+
+        const key = prompt('Qaysi keyni o`zgartirmoqchisiz: ')
+
+        if(!Object.keys(user).includes(key)){
+            return
+        }
+
+        const value = prompt('Qiymatni kiriting: ')
+
+        const newArray = db.users.map((u) => {
+            if(u.id === +id) {
+                    return {
+                        ...u,
+                        [key]: value
+                    }
+            } else {
+                return u
+            }
+        })
+        setDB({id: db.id, users:newArray})
+
+    }
     }
